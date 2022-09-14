ea56087a1573b31b836b0da688f0b4a4c30fe339
Supatsorn Poonsawat <supatsorn.supa@mail.kmutt.ac.th>
2022-09-14 21:08:46 +0700

    concat1

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,17 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  let a = array1 ;
+  let b = array2 ;
+  if(( a == null || a == undefined) && (b == null || b==undefined)){
+    return undefined;
+  }else if(b==null || b== undefined){
+    return a;
+  }else if(a==null || a == undefined){
+    return b;
+  }else{
+    return a.concat(b);
+  }
+
 }
 module.exports = concatArray
